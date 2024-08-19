e3fa911290d47461c3807efe9e90e02aa79a0c39
mapachemirlo <claudiophv@gmail.com>
2024-08-19 11:03:36 -0300

    Se agrega estilos al Home.

--- frontend/hackthedot-dapp/src/components/Home.js
+++ frontend/hackthedot-dapp/src/components/Home.js
@@ -1,20 +1,58 @@
 // src/components/Home.js
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Home.css'; // Asegúrate de crear y enlazar el archivo CSS
 
 const Home = () => {
   return (
-    <div>
-      <h1>Bienvenido a Hack the Dot</h1>
-      <p>¡El desafío está a punto de comenzar!</p>
-      <Link to="/register">
-        <button>Registrarse</button>
-      </Link>
-      <Link to="/login">
-        <button>Iniciar Sesión</button>
-      </Link>
+    <div className="home-container">
+      <header className="home-header">
+        <h1 className="home-title">Hack the dot</h1>
+        <nav className="home-nav">
+          <Link to="/about">About</Link>
+          <Link to="/help">Help</Link>
+          <Link to="/ranking">Ranking</Link>
+        </nav>
+      </header>
+      <main className="home-main">
+        <h2 className="home-tagline">
+          Learn about security, overcome challenges and earn rewards
+        </h2>
+        <div className="home-buttons">
+          <Link to="/register">
+            <button className="register-button">Register</button>
+          </Link>
+          <Link to="/login">
+            <button className="login-button">Login</button>
+          </Link>
+        </div>
+      </main>
+      <footer className="home-footer">
+        <p>By <span className="polkadot">Polkadot</span></p>
+      </footer>
     </div>
   );
 };
 
 export default Home;
+
+// // src/components/Home.js
+// import React from 'react';
+// import { Link } from 'react-router-dom';
+
+// const Home = () => {
+//   return (
+//     <div>
+//       <h1>Bienvenido a Hack the Dot</h1>
+//       <p>¡El desafío está a punto de comenzar!</p>
+//       <Link to="/register">
+//         <button>Registrarse</button>
+//       </Link>
+//       <Link to="/login">
+//         <button>Iniciar Sesión</button>
+//       </Link>
+//     </div>
+//   );
+// };
+
+// export default Home;
--- frontend/hackthedot-dapp/src/firebase.js
+++ frontend/hackthedot-dapp/src/firebase.js
@@ -4,14 +4,22 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore'; // Import Firestore
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDTKibj_ZBbTRve2Lo8n2-voQ9pweM3JnU",
-  authDomain: "hackthedot-cd0eb.firebaseapp.com",
-  projectId: "hackthedot-cd0eb",
-  storageBucket: "hackthedot-cd0eb.appspot.com",
-  messagingSenderId: "574115486311",
-  appId: "1:574115486311:web:921d3be57e7c71220452c2",
-  measurementId: "G-CTZ03DNMWW"
-};
+    apiKey: "AIzaSyBJv0Wn0YDQ8oerB9G80IelhY8r1gtqvlQ",
+    authDomain: "hackthedot-test.firebaseapp.com",
+    projectId: "hackthedot-test",
+    storageBucket: "hackthedot-test.appspot.com",
+    messagingSenderId: "836364621788",
+    appId: "1:836364621788:web:1847c38df1b6fd90da2119",
+    measurementId: "G-ST35PWPQ5C"
+  };
+  // apiKey: "AIzaSyDTKibj_ZBbTRve2Lo8n2-voQ9pweM3JnU",
+  // authDomain: "hackthedot-cd0eb.firebaseapp.com",
+  // projectId: "hackthedot-cd0eb",
+  // storageBucket: "hackthedot-cd0eb.appspot.com",
+  // messagingSenderId: "574115486311",
+  // appId: "1:574115486311:web:921d3be57e7c71220452c2",
+  // measurementId: "G-CTZ03DNMWW"
+//};
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
