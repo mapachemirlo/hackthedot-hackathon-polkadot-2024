3c22230c0bb0bb6cce9bd2479549def02bbbad5b
mapachemirlo <claudiophv@gmail.com>
2024-08-20 16:12:55 -0300

    Se actualizan algunos metodos y archivo firebase.

--- frontend/hackthedot-dapp/src/components/Home.js
+++ frontend/hackthedot-dapp/src/components/Home.js
@@ -39,23 +39,3 @@ const Home = () => {
 
 export default Home;
 
-// // src/components/Home.js
-// import React from 'react';
-// import { Link } from 'react-router-dom';
-
-// const Home = () => {
-//   return (
-//     <div>
-//       <h1>Bienvenido a Hack the Dot</h1>
-//       <p>¡El desafío está a punto de comenzar!</p>
-//       <Link to="/register">
-//         <button>Registrarse</button>
-//       </Link>
-//       <Link to="/login">
-//         <button>Iniciar Sesión</button>
-//       </Link>
-//     </div>
-//   );
-// };
-
-// export default Home;
--- frontend/hackthedot-dapp/src/firebase.js
+++ frontend/hackthedot-dapp/src/firebase.js
@@ -12,34 +12,9 @@ const firebaseConfig = {
     appId: "1:836364621788:web:1847c38df1b6fd90da2119",
     measurementId: "G-ST35PWPQ5C"
   };
-  // apiKey: "AIzaSyDTKibj_ZBbTRve2Lo8n2-voQ9pweM3JnU",
-  // authDomain: "hackthedot-cd0eb.firebaseapp.com",
-  // projectId: "hackthedot-cd0eb",
-  // storageBucket: "hackthedot-cd0eb.appspot.com",
-  // messagingSenderId: "574115486311",
-  // appId: "1:574115486311:web:921d3be57e7c71220452c2",
-  // measurementId: "G-CTZ03DNMWW"
-//};
-
+  
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-export const db = getFirestore(app); // Export Firestore
+export const db = getFirestore(app); 
 
 
-
-// src/firebase.js
-// import { initializeApp } from "firebase/app";
-// import { getAuth } from "firebase/auth";
-
-// const firebaseConfig = {
-//     apiKey: "AIzaSyDTKibj_ZBbTRve2Lo8n2-voQ9pweM3JnU",
-//     authDomain: "hackthedot-cd0eb.firebaseapp.com",
-//     projectId: "hackthedot-cd0eb",
-//     storageBucket: "hackthedot-cd0eb.appspot.com",
-//     messagingSenderId: "574115486311",
-//     appId: "1:574115486311:web:921d3be57e7c71220452c2",
-//     measurementId: "G-CTZ03DNMWW"
-//   };
-
-// const app = initializeApp(firebaseConfig);
-// export const auth = getAuth(app);
